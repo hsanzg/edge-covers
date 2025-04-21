b4a048d4a88480119568885fb84e5d0c82d3b171
Hugo Sanz <80487270+hsanzg@users.noreply.github.com>
2025-04-22 01:55:32 +0200

    Skip the initial zero terms

--- edge_covers.c
+++ edge_covers.c
@@ -10,7 +10,7 @@
 // that $1+8\binom N2$ fits in a `uint`, which needs to be the case if it is
 // to terminate in a reasonable amount of time.
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 int main(const int argc, char *argv[]) {
   if (argc != 2) {
@@ -18,22 +18,17 @@ int main(const int argc, char *argv[]) {
     return 1;
   }
   const uint N = strtoul(argv[1], NULL, 0);
-  mpz_t res, term, k_2_choose_e; mpz_inits(res, term, NULL), mpz_init_set_ui(k_2_choose_e, 1);
+  mpz_t res, n_choose_k, k_2_choose_e; mpz_inits(res, n_choose_k, k_2_choose_e, NULL);
   for (uint n = 1, e_max = 0; n <= N; e_max += n, ++n) {
     for (uint e = (n + 1) / 2; e <= e_max; ++e) {
       uint k = (1 + sqrt(1 + 8 * e)) / 2, k_choose_2 = k * (k - 1) / 2;
       if (k_choose_2 < e) k_choose_2 += k++; // apply ceiling.
       for (; k <= n; k_choose_2 += k++) {
-        for (uint j = 0; j < MIN(k_choose_2 - e, k - 1); ++j) {
-          mpz_mul_ui(k_2_choose_e, k_2_choose_e, k_choose_2 - j);
-          mpz_divexact_ui(k_2_choose_e, k_2_choose_e, k_choose_2 - j - e);
-        }
-        //mpz_bin_uiui(k_2_choose_e, k_choose_2, e);
-        mpz_bin_uiui(term, n, k);
-        ((n - k) & 1 ? mpz_submul : mpz_addmul)(res, term, k_2_choose_e);
+        mpz_bin_uiui(n_choose_k, n, k);
+        mpz_bin_uiui(k_2_choose_e, k_choose_2, e);
+        ((n - k) & 1 ? mpz_submul : mpz_addmul)(res, n_choose_k, k_2_choose_e);
       }
-      mpz_out_str(stdout, 10, res);
-      mpz_set_ui(res, 0), mpz_set_ui(k_2_choose_e, 1);
+      mpz_out_str(stdout, 10, res), mpz_set_ui(res, 0);
       putchar(e < e_max ? ',' : '\n');
     }
   }
